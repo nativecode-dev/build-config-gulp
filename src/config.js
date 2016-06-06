22429460f660e9af26afa36ca28c9b39b2f6ab91
Mike Pham <mpham@nativecode.com>
2016-06-06 00:57:11 -0400

    fixing config auto resolve

--- src/config.js
+++ src/config.js
@@ -1,9 +1,23 @@
 module.exports = (plugin, util) => {
+  var resolve = function (hash, root) {
+    root = root || hash
+    Object.keys(hash).map(key => {
+      var value = hash[key]
+      if (value.length && value.indexOf(':') === 0) {
+        var expanded = util.expand(value).with(root).substring(1)
+        hash[key] = expanded
+      } else if (value instanceof Object) {
+        resolve(value, root)
+      }
+    })
+    return root
+  }
   return {
-    load: filename => {
+    load: (filename, asis) => {
       var filepath = util.path.join(process.cwd(), filename || 'gulpfile.json')
       if (util.fs.statSync(filepath)) {
-        return JSON.parse(util.fs.readFileSync(filepath))
+        var json = JSON.parse(util.fs.readFileSync(filepath))
+        return asis ? json : resolve(json)
       }
     }
   }
